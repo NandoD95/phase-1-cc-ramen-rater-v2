f7d96a70e27081070de7a1f298e49514465c8ba4
Fernando De Jesus <fdejesus95@outlook.com>
2024-04-17 16:13:05 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -29,11 +29,16 @@
 // };
 
 fetch ('http://localhost:3000/ramens')
-  .then ((resp) => resp.json())
-  .then (data => renderRamen(data))
+  .then (resp => resp.json())
+  .then (data => {
+    renderRamen(data)
+    if (data.length > 0){
+      handleClick(data[0])
+    }
+  })
 
-function renderRamen(ramenArr){ 
 
+function renderRamen(ramenArr){
   const ramenMenu = document.querySelector('#ramen-menu')
   // console.log(ramenMenu)
   ramenArr.forEach((ramenObj) => {
@@ -42,26 +47,26 @@ function renderRamen(ramenArr){
    // console.log(img)
     img.src = ramenObj.image
     img.addEventListener('click',() => handleClick(ramenObj))
-
-    function handleClick(ramenObj){
-      const ramenDetail = document.querySelector('#ramen-detail')
-      const img = document.querySelector('.detail-image')
-      img.src = ramenObj.image
-
-      const h2 = document.querySelector('.name') 
-      h2.textContent = ramenObj.name
-
-      const restaurant = document.querySelector('.restaurant')
-      restaurant.textContent = ramenObj.restaurant
-
-      const rating = document.querySelector('#rating-display')
-      rating.textContent = ramenObj.rating
-
-      const comment = document.querySelector('#comment-display')
-      comment.textContent = ramenObj.comment
-    }
     ramenMenu.appendChild(img)
     })
+}
+
+  function handleClick(ramenObj){
+    const ramenDetail = document.querySelector('#ramen-detail')
+    const img = document.querySelector('.detail-image')
+    img.src = ramenObj.image
+
+    const h2 = document.querySelector('.name') 
+    h2.textContent = ramenObj.name
+
+    const restaurant = document.querySelector('.restaurant')
+    restaurant.textContent = ramenObj.restaurant
+
+    const rating = document.querySelector('#rating-display')
+    rating.textContent = ramenObj.rating
+
+    const comment = document.querySelector('#comment-display')
+    comment.textContent = ramenObj.comment
   }
 
   const form = document.querySelector('#new-ramen')
@@ -81,4 +86,20 @@ function renderRamen(ramenArr){
       comment : document.querySelector('#new-comment').value,
     }
     renderRamen([newRamen])
+  }
+
+  const editForm = document.querySelector('#edit-ramen')
+  editForm.addEventListener('submit', (e) => handleEditRamen(e))
+
+  function handleEditRamen(e) {
+    e.preventDefault();
+    const newRating = e.target.rating.value;
+    const newComment = document.querySelector('#updated-comment').value;
+    // console.log(newComment)
+
+    const ratingDisplay = document.querySelector('#rating-display');
+    const commentDisplay = document.querySelector('#comment-display');
+
+    ratingDisplay.textContent = newRating;
+    commentDisplay.innerHTML = newComment;
   }
